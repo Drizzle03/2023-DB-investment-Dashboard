44d33957fdab9f0d92ba3333a9481cb018a7795f
drizzle03 <tori1652@naver.com>
2023-06-08 08:21:41 +0900

    대시보드 구축 초기 틀

---
+++ Post.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import yfinance as yf
+from pandas_datareader import data as pdr
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime, timedelta
+
+yf.pdr_override()
+
+# Define the scope
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+# Add your service account file
+creds = ServiceAccountCredentials.from_json_keyfile_name('db-investment-1c34891f7025.json', scope)
+
+# Authorize the clientsheet 
+client = gspread.authorize(creds)
+
+# Get the instance of the Spreadsheet
+sheet = client.open('DB-Dashboard') 
+
+# List of your tickers
+tickers = ['^KS11', '^KQ11', '^GSPC', '^STOXX50E', '^N225', '000300.SS', '^TNX', 'LQD', 'BNDX', 'GLD', 'CL=F', 'SHY']
+
+# Define date range
+end_date = datetime.now()
+start_date = end_date - timedelta(days=30)
+
+# Get data and upload to Google Sheets
+for ticker in tickers:
+    data = pdr.get_data_yahoo(ticker, start_date, end_date)
+    # Create a new worksheet with the name of today's date
+    date_today = datetime.now().strftime("%Y.%m.%d")
+    try:
+        worksheet = sheet.add_worksheet(title=date_today, rows="100", cols="20")
+    except gspread.exceptions.APIError:
+        worksheet = sheet.worksheet(date_today)
+    # Get the data and transpose it to columns
+    data_to_upload = data['Close'].transpose()
+    # Use the .set method and the list of lists variable to update the sheet
+    worksheet.insert_rows(data_to_upload.values.tolist(), row=1)
